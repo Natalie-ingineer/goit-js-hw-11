8f6f355790b9faf9cc3b3d59746a2a94aa99dcf6
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-07 21:54:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41079066-0341c17d8bd684537c8a66e3e';
 let page = 1;
 
-const queryParams = new URLSearchParams({
-  q: 'cat',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-  page: page,
-  per_page: 40,
-});
+// const queryParams = new URLSearchParams({
+//   key: API_KEY,
+//   q: '',
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: 'true',
+//   page: page,
+//   per_page: 40,
+// });
 
 const searchForm = document.querySelector('.search-form');
 const btnSubmit = document.querySelector('button');
@@ -28,19 +29,77 @@ function handlerSearch(event) {
     .getAll('animal')
     .filter(item => item)
     .map(item => item.trim());
-  getAnimals(arr);
+  getAnimals(arr)
+    .then(async resp => {
+      const animals = resp.map(({ origin }) => origin[0]);
+      const animalsService = await getAnimals(animals);
+      divGallery.innerHTML = createMarkup(animalsService);
+    })
+    .catch(e => console.log(e))
+    .finally(() => searchForm.reset());
 }
 
 async function getAnimals(arr) {
-  const resps = arr.map(async item => {
-    const resp = await fetch(`${BASE_URL}?API_KEY&${queryParams}&${item}`);
+  const resps = arr.map(async animal => {
+    const queryParams = new URLSearchParams({
+      key: API_KEY,
+      q: animal,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: page,
+      per_page: 40,
+    });
+
+    const resp = await fetch(`${BASE_URL}?${queryParams}&${animal}`);
     if (!resp.ok) {
       throw new Error();
     }
     return await resp.json();
   });
+
   const data = await Promise.allSettled(resps);
-  console.log(data);
+  const animalObj = data
+    .filter(({ status }) => status === 'fulfilled')
+    .map(({ value }) => value[0]);
+
+  return animalObj;
+}
+
+// async function getAnimals(arr) {
+//   const resps = arr.map(async item => {
+//     const resp = await fetch(`${BASE_URL}?${queryParams}&${item}`);
+//     if (!resp.ok) {
+//       throw new Error();
+//     }
+//     return await resp.json();
+//   });
+//   const data = await Promise.allSettled(resps);
+//   const animalObj = data
+//     .filter(({ status }) => status === 'fulfilled')
+//     .map(({ value }) => value[0]);
+
+//   return animalObj;
+// }
+
+function createMarkup() {
+  `<div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+      </p>
+    </div>
+  </div>;`;
 }
 
 function handlerSubmit() {}
